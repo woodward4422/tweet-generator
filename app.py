d7b3f382fc7e41b53aac2e1946d37d523e8d0a72
noah <noahwoodward2244@gmail.com>
2018-11-03 13:16:16 -0700

    Allowed for Query Parameters for Flask app

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask 
+from flask import request
 app = Flask(__name__)
 import weighted_random_test 
 
 @app.route('/')
 def get_string():
+    total_words = int(request.args.get('words'))
     total_string = ""
     words = weighted_random_test.open_file()
     histo = weighted_random_test.histogram(words)
 
-    for _ in range(6):
+    for _ in range(total_words):
         rand_word = weighted_random_test.random_word(histo)
         total_string = total_string + " " +  rand_word
     return total_string
