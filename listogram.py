3f8e86f76207fa3ed41381887aca0ae54d4d73e8
noah <noahwoodward2244@gmail.com>
2018-11-07 12:30:10 -0800

    Finished Dictogram

--- listogram.py
+++ listogram.py
@@ -19,20 +19,47 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
+       
         # TODO: Increase word frequency by count
+        for element in self:
+            print("In Here!")
+            if word == element[0]:
+                element[1] += 1
+            else:
+                self.append([word,1])
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for element in self:
+            if element[0] == word:
+                return element[1]
+            else:
+                continue
+        return 0 
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for element in self:
+            if element[0] == word:
+                return True 
+            else:
+                continue
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        index = 0 
+        for element in self: 
+            if element[0] == target:
+                return index
+            else: 
+                index += 1
+                continue
+        return None
 
 
 def print_histogram(word_list):
