4d30d7ad54c8c3a1d4fe05c6fecda98b05e9208e
noah <noahwoodward2244@gmail.com>
2018-11-07 19:06:19 -0800

    Reminder to fix weighted random test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import weighted_random_test
 
 @app.route('/')
 def get_string():
-    total_words = int(request.args.get('words'))
+    total_words = int(request.args.get('words')) 
     total_string = ""
     words = weighted_random_test.open_file()
     histo = weighted_random_test.histogram(words)
@@ -14,3 +14,10 @@ def get_string():
         rand_word = weighted_random_test.random_word(histo)
         total_string = total_string + " " +  rand_word
     return total_string
+
+# TODO: Add terminal args functionality to run the script
+# script = argv[0]
+# try:
+#     arg1 = argv[1]
+# except:
+#     arg1 = ''
--- sample.py
+++ sample.py
@@ -15,8 +15,23 @@ def weighted_random_word(histogram_dict):
             return key
         else: 
             continue
-# TODO: Add a tested function to test the weighted selection
+# TODO: Add a tester function to test the weighted selection
+
+def weighted_random_test():
+    ''' Tests to make sure that the weighted random sampling is correct ''' 
+    histogram = word_count.create_histogram('fish.txt')
+    for _ in range(0,10000):
+        result_dict = {}
+        word_selected = weighted_random_word(histogram)
+        print(word_selected)
+        if word_selected in result_dict:
+            result_dict[word_selected] += 1
+        else:
+            result_dict[word_selected] = 1
+    print(result_dict)
+    return result_dict
 
 if __name__ == "__main__":
     print(weighted_random_word(word_count.create_histogram('fish.txt')))
+    weighted_random_test()
 
